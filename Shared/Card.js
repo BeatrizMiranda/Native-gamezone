a1caf63eea3982484385dc473250f56c65f956a1
Beatriz Miranda <beatriz.miranda@acct.global>
2020-03-06 10:00:57 -0300

    card style

--- Shared/Card.js
+++ Shared/Card.js
@@ -3,13 +3,25 @@ import { StyleSheet, View, Text } from "react-native";
 
 export default Card = props => {
     return (
-        <View style={styles.card}>
-            <View style={styles.cardContent}>{props.children}</View>
+        <View style={style.card}>
+            <View style={style.cardContent}>{props.children}</View>
         </View>
     );
 };
 
 const style = StyleSheet.create({
-    card: {},
-    cardContent: {}
+    card: {
+        borderRadius: 6,
+        elevation: 3,
+        backgroundColor: "#fff",
+        shadowOffset: { width: 1, height: 1 },
+        shadowColor: "#333",
+        shadowOpacity: 0.3,
+        margin: 5,
+        shadowRadius: 2
+    },
+    cardContent: {
+        marginHorizontal: 18,
+        marginVertical: 10
+    }
 });
