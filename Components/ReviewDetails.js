84ec03a9ec68920bfe76522112c8f76ff2348af6
Beatriz Miranda <beatriz.miranda@acct.global>
2020-03-06 10:39:18 -0300

    trying to fix global

--- Components/ReviewDetails.js
+++ Components/ReviewDetails.js
@@ -1,20 +1,30 @@
 import React from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
-import globalStyle from "../Styles/global.js";
+import { Text, View, StyleSheet, Image } from "react-native";
+import globalStyles from "../Styles/global.js";
+import Card from "../Shared/Card.js";
 
 export default function ReviewDetails({ route, navigation }) {
     // const goBack = () => {
     //     navigation.goBack();
     // };
+    const rating = route.params.item.rating;
     console.log("route", route);
+    console.log("route stars", route.params.item.rating);
     return (
-        <View style={globalStyle.container}>
-            <Text>{route.params.item.title}</Text>
-            <Text>{route.params.item.body}</Text>
-            <Text>{route.params.item.rating}</Text>
-            {/* <Button title="Home" onPress={goBack} /> */}
+        <View style={globalStyles.container}>
+            <Card>
+                <Text>{route.params.item.title}</Text>
+                <Text>{route.params.item.body}</Text>
+                <View style={styles.rating}>
+                    <Text>Gamezone Rating: </Text>
+                    {/* <Image source={require(images.ratings[rating])} /> */}
+                </View>
+                {/* <Button title="Home" onPress={goBack} /> */}
+            </Card>
         </View>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    rating: {}
+});
--- Styles/global.js
+++ Styles/global.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
 
-export default StyleSheet.create({
+const globalStyles = StyleSheet.create({
     container: {
         padding: 20,
         flex: 1
@@ -15,3 +15,15 @@ export default StyleSheet.create({
         color: "#333"
     }
 });
+
+export default globalStyles;
+
+// export const images = {
+//     ratings: {
+//         "1": require("../assets/img/rating-1.png"),
+//         "2": require("../assets/img/rating-2.png"),
+//         "3": require("../assets/img/rating-3.png"),
+//         "4": require("../assets/img/rating-4.png"),
+//         "5": require("../assets/img/rating-5.png")
+//     }
+// };
